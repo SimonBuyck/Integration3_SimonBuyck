8a21fc5de9a54bd51e06a548e40183b4d311be70
Simon Buyck <simon.buyck@student.howest.be>
2021-01-21 13:33:04 +0100

    shop html & css

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import "./js/KNN";
     startTimer();
 
     let scrEl = document.getElementById("scr-el");
-    scrEl.addEventListener("scroll", (event) => {
-      let scrolled =
-        (scrEl.scrollLeft / (scrEl.scrollWidth - scrEl.clientWidth)) * 100;
-      document.getElementById("myBar").style.width = scrolled + "%";
-    });
+    if(scrEl){
+      scrEl.addEventListener("scroll", (event) => {
+        let scrolled =
+          (scrEl.scrollLeft / (scrEl.scrollWidth - scrEl.clientWidth)) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+      });
+    }
   };
 
   const startTimer = () => {
@@ -34,20 +36,22 @@ import "./js/KNN";
       var hoursSpan = document.querySelector(".hours");
       var minutesSpan = document.querySelector(".minutes");
 
-      function updateClock() {
-        var t = getTimeRemaining(endtime);
+      if (daysSpan || hoursSpan || minutesSpan){
+        function updateClock() {
+          var t = getTimeRemaining(endtime);
 
-        daysSpan.innerHTML = t.days;
-        hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-        minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+          daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+          minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
 
-        if (t.total <= 0) {
-          clearInterval(timeinterval);
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
         }
-      }
 
-      updateClock();
-      var timeinterval = setInterval(updateClock, 1000);
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+      }
     }
 
     var deadline = Date.parse("April 30, 2021");
