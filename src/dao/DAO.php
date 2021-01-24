a861979beb205ccbe83f248163b8ba97d19f90fa
Simon Buyck <simon.buyck@student.howest.be>
2021-01-25 00:23:50 +0100

    make it better

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -11,10 +11,10 @@ class DAO {
 
 		if(empty(self::$sharedPDO)) {
 
-      $dbHost = getenv('PHP_DB_HOST') ?: "localhost";
-      $dbName = getenv('PHP_DB_DATABASE') ?: "todos";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "todos";
-      $dbPass = getenv('PHP_DB_PASSWORD') ?: "todos";
+      $dbHost = getenv('PHP_DB_HOST') ?: "ID282217_maker.db.webhosting.be";
+      $dbName = getenv('PHP_DB_DATABASE') ?: "ID282217_maker";
+      $dbUser = getenv('PHP_DB_USERNAME') ?: "ID282217_maker";
+      $dbPass = getenv('PHP_DB_PASSWORD') ?: "AzErTy0987";
 
 			self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
 			self::$sharedPDO->exec("SET CHARACTER SET utf8");
