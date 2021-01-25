a9abbff6213812c437af6c4a5ba2bea56187ac8b
Simon Buyck <simon.buyck@student.howest.be>
2021-01-25 17:46:33 +0100

    changes in development before merge

---
+++ src/view/tutorial/tutorial_slede.php
@@ -0,0 +1,173 @@
+<main class="tutorial">
+  <div class="webcam" id="videoContainer">
+    <canvas width="640" height="480" id="myCanvas"></canvas>
+  </div>
+  <div id="scr-el" class="landscape tutorial__content">
+    <ul class="tutorial__items">
+      <li class="plank1">plank</li>
+      <li class="plank2">plank</li>
+      <li class="plank3">plank</li>
+      <li class="deuvel">deuvel</li>
+      <li class="deuvelpin">deuvelpin</li>
+      <li class="meter">meter</li>
+      <li class="screw">screw</li>
+      <li class="boormachine">boormachine</li>
+      <li class="borstel">borstel</li>
+      <li class="zaag">zaag</li>
+      <li class="verf">verf</li>
+      <li class="houtlijm">houtlijm</li>
+      <li class="vernis">vernis</li>
+    </ul>
+    <div class="tutorial__maten">
+      <div class="tutorial__maten__intro">
+        <img class="tutorial__maten__img--ondersteuning" src="./assets/img/zitondersteuning.png" alt="zitondersteuning van de slede" sizes="383px" srcset="
+                ./assets/img/zitondersteuning@0.5x.png  219w,
+                ./assets/img/zitondersteuning@2x.png    764w,
+                ./assets/img/zitondersteuning@3x.png   1145w
+              " />
+        <p>
+          We gaan van start met het afmeten van de verschillende stukken
+          hout
+        </p>
+      </div>
+      <img class="tutorial__maten__img--stokken" src="./assets/img/stokken.png" alt="stokken" sizes="439px" srcset="
+              ./assets/img/stokken@0.5x.png  191w,
+              ./assets/img/stokken@2x.png    878w,
+              ./assets/img/stokken@3x.png   1316w
+            " />
+      <img class="tutorial__maten__img--poten" src="./assets/img/poten.png" alt="poten van de slede" sizes="595px" srcset="
+              ./assets/img/poten@0.5x.png  297w,
+              ./assets/img/poten@2x.png   1189w,
+              ./assets/img/poten@3x.png   1783w
+            " />
+    </div>
+    <div class="tutorial__zagen">
+      <img class="tutorial__zagen__zaag" src="./assets/img/tutorial_zaag.png" alt="een zaag" sizes="452px" srcset="
+              ./assets/img/tutorial_zaag@0.5x.png  226w,
+              ./assets/img/tutorial_zaag@2x.png    904w,
+              ./assets/img/tutorial_zaag@3x.png   1356w
+            " />
+      <div class="tutorial__zagen__text">
+        <p>Zaag nu de stukken op je getekende lijnen.</p>
+        <p>TIP: Zaag de gebogen lijnen met een figuurzaag.</p>
+      </div>
+    </div>
+    <div class="tutorial__lijmen">
+      <div class="wood wood--top"></div>
+      <div class="tutorial__lijmen__text">
+        <p>Zaag nu de stukken op je getekende lijnen.</p>
+        <p>TIP: Zaag de gebogen lijnen met een figuurzaag.</p>
+      </div>
+      <div class="wood wood--bottom"></div>
+    </div>
+    <div class="tutorial__wait">
+      <img src="./assets/img/Group 34.png" class="cycle" />
+    </div>
+    <div class="tutorial__deuvel">
+      <img class="tutorial__deuvel__img" src="./assets/img/deuvel.png" alt="deuvel" sizes="72px" srcset="
+              ./assets/img/deuvel.png  36w,
+              ./assets/img/deuvel.png 143w,
+              ./assets/img/deuvel.png 215w
+            " />
+      <p class="tutorial__deuvel__text">
+        boor gaten die de helft van de deuvel diep zijn en tel er nog +-2
+        milimeter bij. Dit zodat de twee stukken straks goed op elkaar
+        passen.
+      </p>
+    </div>
+    <div class="tutorial__boren">
+      <img class="tutorial__boren__drill" src="./assets/img/boormachine.png" alt="boormachine" sizes="404px" srcset="
+              ./assets/img/boormachine@0.5x.png  202w,
+              ./assets/img/boormachine@2x.png    806w,
+              ./assets/img/boormachine@3x.png   1210w
+            " />
+      <div class="tutorial__boren__wood">
+        <div class="tutorial__boren__deuvel__hole"></div>
+        <div class="tutorial__boren__deuvel__hole"></div>
+        <div class="tutorial__boren__deuvel__hole"></div>
+      </div>
+    </div>
+    <div class="tutorial__deuvelpin">
+      <img class="tutorial__deuvel__img" src="./assets/img/deuvelpin.png" alt="deuvel" sizes="72px" srcset="
+              ./assets/img/deuvelpin@0.5x.png  36w,
+              ./assets/img/deuvelpin@2x.png   143w,
+              ./assets/img/deuvelpin@3x.png   215w
+            " />
+      <p class="tutorial__deuvel__text">
+        Plaats de deuvelpinnen in de juist geboorde gatten. Duw nu het
+        zitvlakondersteuningsstuk hard zodat er putjes in het hout komen.
+        Boor in de putjes ook gaatjes.
+      </p>
+    </div>
+    <div class="tutorial__assembleren">
+      <img class="tutorial__assembleren__img" src="./assets/img/assembleren.png" alt="assembleren" sizes="72px" srcset="
+              ./assets/img/assembleren@0.5x.png  36w,
+              ./assets/img/assembleren@2x.png   143w,
+              ./assets/img/assembleren@3x.png   215w
+            " />
+      <p class="tutorial__assembleren__text">
+        doe wat lijm in de geboorde gaatjes en duw de deuvels erin. lijm de
+        zijkant van de zitvlakondersteuning in. Doe hetzelfde bij de poten.
+      </p>
+    </div>
+    <div class="tutorial__wait">
+      <img src="./assets/img/Group 34.png" class="cycle" />
+    </div>
+    <div class="tutorial__stokken">
+      <p class="tutorial__stokken__text">
+        Vijs de stokken vast op het zitvlak. De drie korte in het midden en
+        de twee lange langst de buitenkant.
+      </p>
+      <img class="tutorial__stokken__img" src="./assets/img/slede_top_1.png" alt="stokken aan de slede hangen" sizes="251px" srcset="
+              ./assets/img/slede_top_1@0.5x.png 126w,
+              ./assets/img/slede_top_1@2x.png   502w,
+              ./assets/img/slede_top_1@3x.png   753w
+            " />
+    </div>
+    <div class="tutorial__vernis">
+      <p class="tutorial__stokken__text">
+        Vijs de stokken vast op het zitvlak. De drie korte in het midden en
+        de twee lange langst de buitenkant.
+      </p>
+      <img class="tutorial__stokken__img" src="./assets/img/slede_vernis.png" alt="vernis op een slede" sizes="300px" srcset="
+              ./assets/img/slede_vernis@0.5x.png 150w,
+              ./assets/img/slede_vernis@2x.png   600w,
+              ./assets/img/slede_vernis@3x.png   900w
+            " />
+    </div>
+    <div class="tutorial__paint">
+      <img class="tutorial__paint__img" src="./assets/img/slede_1.png" alt="slede" sizes="544px" srcset="
+              ./assets/img/slede_1@0.5x.png  272w,
+              ./assets/img/slede_1@2x.png   1088w,
+              ./assets/img/slede_1@3x.png   1632w
+            " />
+      <p class="tutorial__paint__text">
+        Vijs de stokken vast op het zitvlak. De drie korte in het midden en
+        de twee lange langst de buitenkant.
+      </p>
+      <p class="button">
+        <a class="button__link" href="#">Ik begrijp het</a>
+      </p>
+      <div class="tutorial__paint__paint">
+        <img src="./assets/img/verfpot_rood.png" alt="rode verfpot" sizes="627px" srcset="
+                ./assets/img/verfpot_rood@0.5x.png  313w,
+                ./assets/img/verfpot_rood@2x.png   1253w,
+                ./assets/img/verfpot_rood@3x.png   1879w
+              " />
+        <img src="./assets/img/verfpot_groen.png" alt="groene verfpot" sizes="627px" srcset="
+                ./assets/img/verfpot_groen@0.5x.png  313w,
+                ./assets/img/verfpot_groen@2x.png   1253w,
+                ./assets/img/verfpot_groen@3x.png   1879w
+              " />
+        <img src="./assets/img/verfpot_blauw.png" alt="rode verfpot" sizes="627px" srcset="
+                ./assets/img/verfpot_blauw@0.5x.png  313w,
+                ./assets/img/verfpot_blauw@2x.png   1253w,
+                ./assets/img/verfpot_blauw@3x.png   1879w
+              " />
+      </div>
+    </div>
+    <div class="progress-container">
+      <div class="progress-bar" id="myBar"></div>
+    </div>
+  </div>
+</main>
